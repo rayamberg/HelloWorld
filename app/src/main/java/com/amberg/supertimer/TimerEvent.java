af2c4f14580418788a2bf38cedd9b5732962d8fa
Ray <rayamberg@gmail.com>
2016-11-30 10:46:44 -0900

    Added TimerEvent class
    Every work or rest period will be a TimerEvent. These TimerEvents will likely be stored in a list and will be processed by the main activity.

---
+++ app/src/main/java/com/amberg/supertimer/TimerEvent.java
@@ -0,0 +1,37 @@
+package com.amberg.supertimer;
+
+/**
+ * This class represents a singular event in the sequence of events that occur when the timer is
+ * running. The event has a duration in milliseconds, however may also be null, indicating that
+ * the event is untimed. This will be useful if the user does not need a particular rest or work
+ * set to be timed. The Activity should handle duration appropriately.
+ */
+
+public class TimerEvent {
+    public enum Type { WORK, REST }
+    Type type;
+    private long duration;
+
+    /* default constructor: duration should be in milliseconds
+     * and type should be one of the Types defined above */
+    TimerEvent(long duration, Type type) {
+        this.duration = duration;
+        this.type = type;
+    }
+
+    public long getDuration() {
+        return this.duration;
+    }
+
+    public Type getType() {
+        return this.type;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+}
